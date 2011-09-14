f1fe00a73a61fb08222341c5540f5e512a07c054
Olaf Lüke <olaf@tinkerforge.com>
2011-09-14 16:49:20 +0200

    limit line length of java examples to 100 characters

--- software/bindings/java/ExampleButtonCallback.java
+++ software/bindings/java/ExampleButtonCallback.java
@@ -9,10 +9,13 @@ public class ExampleButtonCallback {
 	// Note: To make the example code cleaner we do not handle exceptions. Exceptions you
 	//       might normally want to catch are described in the commnents below
 	public static void main(String args[]) throws Exception {
-		IPConnection ipcon = new IPConnection(host, port); // Create connection to brickd (Can throw IOException)
+		// Create connection to brickd
+		IPConnection ipcon = new IPConnection(host, port); // Can throw IOException
 
 		BrickletLCD16x2 lcd = new BrickletLCD16x2(UID); // Create device object
-		ipcon.addDevice(lcd); // Add device to ip connection (Can throw IPConnection.TimeoutException)
+
+		// Add device to ip connection
+		ipcon.addDevice(lcd); // Can throw IPConnection.TimeoutException
 		// Don't use device before it is added to a connection
 		
 
