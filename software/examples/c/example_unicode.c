e588e70fc0b29a60805acdbedfa54db27812a6f4
Matthias Bolte <matthias@tinkerforge.com>
2012-06-12 18:57:30 +0200

    Use correct term 'Bricklet' in example code error message

--- software/examples/c/example_unicode.c
+++ software/examples/c/example_unicode.c
@@ -109,7 +109,7 @@ int main() {
 
 	// Add device to IP connection
 	if(ipcon_add_device(&ipcon, &lcd) < 0) {
-		fprintf(stderr, "Could not connect to Brick\n");
+		fprintf(stderr, "Could not connect to Bricklet\n");
 		exit(1);
 	}
 	// Don't use device before it is added to a connection
