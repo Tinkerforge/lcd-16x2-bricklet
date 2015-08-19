95b10eb2a5412624d155752fff597651d49e6adb
Matthias Bolte <matthias@tinkerforge.com>
2015-08-19 15:58:51 +0200

    Cleanup Perl examples

--- software/examples/perl/example_button_callback.pl
+++ software/examples/perl/example_button_callback.pl
@@ -1,36 +1,39 @@
-#!/usr/bin/perl  
+#!/usr/bin/perl
 
 use Tinkerforge::IPConnection;
 use Tinkerforge::BrickletLCD16x2;
 
 use constant HOST => 'localhost';
 use constant PORT => 4223;
-use constant UID => 'jup'; # Change to your UID
-
-# Callback functions for button status
-sub cb_pressed
-{
-    my ($i) = @_;
-
-    print "Pressed: $i\n";
-}
-
-sub cb_released
-{
-    my ($i) = @_;
-
-    print "Realeased: $i\n";
-}
+use constant UID => 'XYZ'; # Change to your UID
 
 my $ipcon = Tinkerforge::IPConnection->new(); # Create IP connection
 my $lcd = Tinkerforge::BrickletLCD16x2->new(&UID, $ipcon); # Create device object
 
+# Callback subroutine for button pressed callback
+sub cb_button_pressed
+{
+    my ($button_pressed) = @_;
+
+    print "Button Pressed: " . $button_pressed . "\n";
+}
+
+# Callback subroutine for button released callback
+sub cb_button_released
+{
+    my ($button_released) = @_;
+
+    print "Button Released: " . $button_released . "\n";
+}
+
 $ipcon->connect(&HOST, &PORT); # Connect to brickd
 # Don't use device before ipcon is connected
 
-# Register button status callbacks to cb_pressed and cb_released
-$lcd->register_callback($lcd->CALLBACK_BUTTON_PRESSED, 'cb_pressed');
-$lcd->register_callback($lcd->CALLBACK_BUTTON_RELEASED, 'cb_released');
+# Register button pressed callback to subroutine cb_button_pressed
+$lcd->register_callback($lcd->CALLBACK_BUTTON_PRESSED, 'cb_button_pressed');
+
+# Register button released callback to subroutine cb_button_released
+$lcd->register_callback($lcd->CALLBACK_BUTTON_RELEASED, 'cb_button_released');
 
 print "Press any key to exit...\n";
 <STDIN>;
--- software/examples/perl/example_hello_world.pl
+++ software/examples/perl/example_hello_world.pl
@@ -1,26 +1,23 @@
-#!/usr/bin/perl  
+#!/usr/bin/perl
 
 use Tinkerforge::IPConnection;
 use Tinkerforge::BrickletLCD16x2;
 
 use constant HOST => 'localhost';
 use constant PORT => 4223;
-use constant UID => 'jup'; # Change to your UID
+use constant UID => 'XYZ'; # Change to your UID
 
 my $ipcon = Tinkerforge::IPConnection->new(); # Create IP connection
 my $lcd = Tinkerforge::BrickletLCD16x2->new(&UID, $ipcon); # Create device object
 
 $ipcon->connect(&HOST, &PORT); # Connect to brickd
-# Don't use device before $ipcon is connected
+# Don't use device before ipcon is connected
 
 # Turn backlight on
 $lcd->backlight_on();
 
-#Clearing display
-$lcd->clear_display();
-
 # Write "Hello World"
-$lcd->write_line(0, 0, 'Hello World');
+$lcd->write_line(0, 0, "Hello World");
 
 print "Press any key to exit...\n";
 <STDIN>;
