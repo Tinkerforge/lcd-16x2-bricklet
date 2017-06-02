a7891d2186a2318032ee04ae87f6191cc7a7af72
Matthias Bolte <matthias@tinkerforge.com>
2017-06-02 16:16:10 +0200

    octave: Use javaObject instead of deprecated java_new

--- software/examples/matlab/octave_example_button_callback.m
+++ software/examples/matlab/octave_example_button_callback.m
@@ -5,8 +5,8 @@ function octave_example_button_callback()
     PORT = 4223;
     UID = "XYZ"; % Change XYZ to the UID of your LCD 16x2 Bricklet
 
-    ipcon = java_new("com.tinkerforge.IPConnection"); % Create IP connection
-    lcd = java_new("com.tinkerforge.BrickletLCD16x2", UID, ipcon); % Create device object
+    ipcon = javaObject("com.tinkerforge.IPConnection"); % Create IP connection
+    lcd = javaObject("com.tinkerforge.BrickletLCD16x2", UID, ipcon); % Create device object
 
     ipcon.connect(HOST, PORT); % Connect to brickd
     % Don't use device before ipcon is connected
