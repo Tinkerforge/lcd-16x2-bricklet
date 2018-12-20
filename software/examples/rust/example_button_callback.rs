f7d5d5058e938218f4b896b3c2cd1f5d2dc2b7f3
Erik Fleckstein <erik@tinkerforge.com>
2018-12-20 16:55:52 +0100

    rust: Reformat examples.

--- software/examples/rust/example_button_callback.rs
+++ software/examples/rust/example_button_callback.rs
@@ -1,43 +1,38 @@
-use std::{io, error::Error};
-use std::thread;
-use tinkerforge::{ip_connection::IpConnection, 
-                  lcd_16x2_bricklet::*};
-
+use std::{error::Error, io, thread};
+use tinkerforge::{ip_connection::IpConnection, lcd_16x2_bricklet::*};
 
 const HOST: &str = "localhost";
 const PORT: u16 = 4223;
 const UID: &str = "XYZ"; // Change XYZ to the UID of your LCD 16x2 Bricklet.
-
-
 
 fn main() -> Result<(), Box<dyn Error>> {
     let ipcon = IpConnection::new(); // Create IP connection.
     let lcd = Lcd16x2Bricklet::new(UID, &ipcon); // Create device object.
 
     ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
-    // Don't use device before ipcon is connected.
+                                          // Don't use device before ipcon is connected.
 
-     let button_pressed_receiver = lcd.get_button_pressed_callback_receiver();
+    let button_pressed_receiver = lcd.get_button_pressed_callback_receiver();
 
-        // Spawn thread to handle received callback messages. 
-        // This thread ends when the `lcd` object
-        // is dropped, so there is no need for manual cleanup.
-        thread::spawn(move || {
-            for button_pressed in button_pressed_receiver {           
-                		println!("Button Pressed: {}", button_pressed);
-            }
-        });
+    // Spawn thread to handle received callback messages.
+    // This thread ends when the `lcd` object
+    // is dropped, so there is no need for manual cleanup.
+    thread::spawn(move || {
+        for button_pressed in button_pressed_receiver {
+            println!("Button Pressed: {}", button_pressed);
+        }
+    });
 
-     let button_released_receiver = lcd.get_button_released_callback_receiver();
+    let button_released_receiver = lcd.get_button_released_callback_receiver();
 
-        // Spawn thread to handle received callback messages. 
-        // This thread ends when the `lcd` object
-        // is dropped, so there is no need for manual cleanup.
-        thread::spawn(move || {
-            for button_released in button_released_receiver {           
-                		println!("Button Released: {}", button_released);
-            }
-        });
+    // Spawn thread to handle received callback messages.
+    // This thread ends when the `lcd` object
+    // is dropped, so there is no need for manual cleanup.
+    thread::spawn(move || {
+        for button_released in button_released_receiver {
+            println!("Button Released: {}", button_released);
+        }
+    });
 
     println!("Press enter to exit.");
     let mut _input = String::new();
