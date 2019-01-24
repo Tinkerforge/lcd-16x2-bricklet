01746963738439e5d9a33f76629f9a00bedd89a6
Erik Fleckstein <erik@tinkerforge.com>
2019-01-24 14:37:55 +0100

    go: Minor example fixes.

--- software/examples/go/example_unicode.go
+++ software/examples/go/example_unicode.go
@@ -2,10 +2,9 @@ package main
 
 import (
 	"fmt"
-    "github.com/Tinkerforge/go-api-bindings/ipconnection"
-    "github.com/Tinkerforge/go-api-bindings/lcd_16x2_bricklet"
+	"github.com/Tinkerforge/go-api-bindings/ipconnection"
+	"github.com/Tinkerforge/go-api-bindings/lcd_16x2_bricklet"
 )
-
 
 const ADDR string = "localhost:4223"
 const UID string = "XYZ" // Change XYZ to the UID of your LCD 16x2 Bricklet.
@@ -109,14 +108,13 @@ func UTF8ToKS0066U(utf8 string) string {
 	return string(result)
 }
 
-
 func main() {
 	ipcon := ipconnection.New()
-    defer ipcon.Close()
+	defer ipcon.Close()
 	lcd, _ := lcd_16x2_bricklet.New(UID, &ipcon) // Create device object.
 
 	ipcon.Connect(ADDR) // Connect to brickd.
-    defer ipcon.Disconnect()
+	defer ipcon.Disconnect()
 	// Don't use device before ipcon is connected.
 
 	// Turn backlight on
@@ -130,6 +128,4 @@ func main() {
 
 	fmt.Print("Press enter to exit.")
 	fmt.Scanln()
-
-	ipcon.Disconnect()
 }
