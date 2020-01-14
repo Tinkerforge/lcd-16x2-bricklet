43bd566a8af67760600dde768bc71a20169de5e3
Erik Fleckstein <erik@tinkerforge.com>
2020-01-14 13:29:00 +0100

    examples: Use python3 input by default.

--- software/examples/python/example_unicode.py
+++ software/examples/python/example_unicode.py
@@ -111,5 +111,5 @@ if __name__ == "__main__":
     # Write a string directly including characters from the LCD charset
     lcd.write_line(1, 0, b'Drehzahl: 1000s\xe9')
 
-    raw_input("Press key to exit\n") # Use input() in Python 3
+    input("Press key to exit\n") # Use raw_input() in Python 2
     ipcon.disconnect()
