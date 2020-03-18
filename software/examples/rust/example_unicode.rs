8ac202e72a824ab31e8a1f768cf76269c24eec75
Matthias Bolte <matthias@tinkerforge.com>
2020-03-18 14:07:09 +0100

    rust: Update to new skeleton

--- software/examples/rust/example_unicode.rs
+++ software/examples/rust/example_unicode.rs
@@ -2,9 +2,9 @@ use std::{error::Error, io};
 
 use tinkerforge::{ip_connection::IpConnection, lcd_16x2_bricklet::*};
 
-const HOST: &str = "127.0.0.1";
+const HOST: &str = "localhost";
 const PORT: u16 = 4223;
-const UID: &str = "XYZ"; // Change XYZ to the UID of your LCD 16x2 Bricklet
+const UID: &str = "XYZ"; // Change XYZ to the UID of your LCD 16x2 Bricklet.
 
 /// Maps a normal UTF-8 encoded string to the LCD charset.
 fn utf8_to_ks0066u(utf8: &str) -> String {
@@ -74,11 +74,11 @@ fn utf8_to_ks0066u(utf8: &str) -> String {
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let ipcon = IpConnection::new(); // Create IP connection
-    let lcd = Lcd16x2Bricklet::new(UID, &ipcon); // Create device object
+    let ipcon = IpConnection::new(); // Create IP connection.
+    let lcd = Lcd16x2Bricklet::new(UID, &ipcon); // Create device object.
 
-    ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd
-                                          // Don't use device before ipcon is connected
+    ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
+                                          // Don't use device before ipcon is connected.
 
     // Turn backlight on
     lcd.backlight_on();
